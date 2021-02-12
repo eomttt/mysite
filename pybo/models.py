d772299e279fdfd75513ceb36f0655ef880e9c53
eomttt <hyunt0413@gmail.com>
2021-02-12 15:51:52 +0900

    Make model & data

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+  subject = models.CharField(max_length=200)
+  content = models.TextField()
+  create_date = models.DateField()
+
+  def __str__(self):
+    return self.subject
+
+class Answer(models.Model):
+  # on_delete=models.CASCADE 는 답변에 연결된 질문이 삭제되면 답변도 함께 삭제하라는 의미
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  content = models.TextField()
+  create_date = models.DateTimeField()
