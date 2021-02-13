5b4f8199e27bf335f061e5270b5b13ad9fc12602
eomttt <hyunt0413@gmail.com>
2021-02-13 22:47:18 +0900

    URL 별칭 설정

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name='pybo'
+
 urlpatterns = [
   # path('', views.index),
   # path('<int:question_id>/', views.detail)
 
   # Generic View
-  path('', views.IndexView.as_view()),
-  path('<int:pk>/', views.DetailView.as_view())
+  path('', views.IndexView.as_view(), name='index'),
+  path('<int:pk>/', views.DetailView.as_view(), name='detail')
 ]
