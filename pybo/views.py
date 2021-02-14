5004977b32cb3f22d93522fd113c8f31531c3b9a
eomttt <hyunt0413@gmail.com>
2021-02-14 20:46:53 +0900

    [02-6] 답변 등록 기능 만들기

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views import generic
-from .models import Question
+from django.utils import timezone
+from .models import Answer, Question
 
 # Create your views here.
 def index(request):
@@ -13,6 +14,15 @@ def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   context = {'question': question}
   return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+
+  # Save by question.answer_set
+  # question.answer_set.create(content=request.POST.get('content', create_date=timezone.now()))
+  answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+  answer.save()
+  return redirect('pybo:detail', question_id=question_id)
 
 # Generic View
 class IndexView(generic.ListView):
