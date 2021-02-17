20b7dc08c6f8a6614a5688aaf33ce64ed83abb2f
eomttt <hyunt0413@gmail.com>
2021-02-17 23:59:24 +0900

    [03-5] 로그인화면 구현

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'pybo.apps.PyboConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# Login success url
+LOGIN_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from pybo import views
 
 urlpatterns = [
+    path('', views.index, name='index'), # / 페이지에 해당하는 urlpatterns
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls'))
 ]
