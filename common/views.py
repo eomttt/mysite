8051d26d7c8c20f6b9b63c6ce0a93fa3e66a2eeb
eomttt <hyunt0413@gmail.com>
2021-02-23 00:52:04 +0900

    [3-06] 회원가입

--- common/views.py
+++ common/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .form import UserForm
+
+def signup(request):
+  if request.method == 'POST':
+    form = UserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      # form.cleaned_data.get 함수는 회원가입 화면에서 입력한 값을 얻기 위해 사용하는 함수이다
+      username = form.cleaned_data.get('username')
+      raw_password = form.cleaned_data.get('password1')
+      user = authenticate(username=username, password=raw_password)
+      login(request, user)
+      return redirect('index')
+  else:
+    form = UserForm()
+  
+  return render(request, 'common/signup.html', { 'form': form })
