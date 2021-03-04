7a342d64a764e4964fe9a1864456871fd7fe94b8
eomttt <hyunt0413@gmail.com>
2021-03-05 00:37:03 +0900

    [3-09] 답글 수정

--- pybo/views.py
+++ pybo/views.py
@@ -18,12 +18,12 @@ def index(request):
   paginator = Paginator(question_list, 10)
   page_obj = paginator.get_page(page)
 
-  context = {'question_list': page_obj}
+  context = { 'question_list': page_obj }
   return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
-  context = {'question': question}
+  context = { 'question': question }
   return render(request, 'pybo/question_detail.html', context)
 
 @login_required(login_url='common:login')
@@ -44,6 +44,27 @@ def answer_create(request, question_id):
   
   context = { 'question': question, 'form': form }
   return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+  answer = get_object_or_404(Answer, pk=answer_id)
+
+  if request.user != answer.author:
+    messages.error(request, 'Not have authorization.')
+    return redirect('pybo:detail', question_id=answer.question.id)
+  
+  if request.method == 'POST':
+    form = AnswerForm(request.POST, instance=answer)
+    if form.is_valid():
+      answer = form.save(commit=False)
+      answer.modify_date = timezone.now()
+      answer.save()
+      return redirect('pybo:detail', question_id=answer.question.id)
+  else:
+    form = AnswerForm(instance=answer)
+  
+  context = { 'form': form }
+  return render(request, 'pybo/answer_form.html', context)
 
 @login_required(login_url='common:login')
 def question_create(request):
