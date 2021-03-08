ba3c6cad24fda0ac511510f2ee7fb5e4f0ad5b64
eomttt <hyunt0413@gmail.com>
2021-03-08 23:49:56 +0900

    [3-10] 답글에 댓글기능 추가

--- pybo/views.py
+++ pybo/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import Answer, Comment, Question
-from .form import AnswerForm, QuestionForm, CommentForm
+from .form import AnswerForm, CommentForm, QuestionForm
 
 # Create your views here.
 def index(request):
@@ -175,6 +175,57 @@ def comment_delete_question(request, comment_id):
     comment.delete()
 
   return redirect('pybo:detail', question_id=comment.question.id)
+
+@login_required(login_url='common:login')
+def comment_create_answer(request, answer_id):
+  answer = get_object_or_404(Answer, pk=answer_id)
+
+  if request.method == 'POST':
+    form = CommentForm(request.POST)
+    if (form.is_valid()):
+      comment = form.save(commit=False)
+      comment.author = request.user
+      comment.create_date = timezone.now()
+      comment.answer = answer
+      comment.save()
+      return redirect('pybo:detail', question_id=answer.question.id)
+  else:
+    form = CommentForm()
+
+  context = { 'form': form }
+  return render(request, 'pybo/comment_form.html', context)
+
+@login_required(login_url='common:login')
+def comment_modify_answer(request, comment_id):
+  comment = get_object_or_404(Comment, pk=comment_id)
+
+  if request.user != comment.author:
+    messages.error(request, 'Not have authorization.')
+    return redirect('pybo:detail', question_id=comment.answer.question.id)
+  
+  if request.method == 'POST':
+    form = CommentForm(request.POST, instance=comment)
+    if form.is_valid():
+      comment = form.save(commit=False)
+      comment.modify_date = timezone.now()
+      comment.save()
+      return redirect('pybo:detail', question_id=comment.answer.question.id)
+  else:
+    form = CommentForm(instance=comment)
+  
+  context = { 'form': form }
+  return render(request, 'pybo/comment_form.html', context)
+      
+@login_required(login_url='common:login')
+def comment_delete_answer(request, comment_id):
+  comment = get_object_or_404(Comment, pk=comment_id)
+  
+  if request.user != comment.author:
+    messages.error(request, 'Not have authorization.')
+  else:
+    comment.delete()
+
+  return redirect('pybo:detail', question_id=comment.answer.question.id)
 
 # Generic View
 class IndexView(generic.ListView):
